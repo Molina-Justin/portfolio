e9b40baad4f76d04db8084ba89d5409b3ed630de
JustinMolina <justinmolina@JustinMolina.local>
2023-01-28 21:30:28 -0500

    update projects

--- src/componets/projects/Projects.jsx
+++ src/componets/projects/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import img_1 from "../../assets/img_1.png";
+import img_5 from "../../assets/img_5.png";
+import img_6 from "../../assets/img_6.png";
 
 import "./projects.css";
 
@@ -15,7 +17,6 @@ const Projects = () => {
     
     
                 <div className="project__box">
-
                     <img src={img_1} alt="" />
                     <h3 className="project__title">Optical Character Recognition</h3>
                     <div className="project_made">
@@ -35,22 +36,53 @@ const Projects = () => {
                         project aimed to make it easy for users to extract 
                         text from images, making it more convenient to edit, 
                         search or analyze the extracted text.
-
-
-
-</span>
+                    </span>
                 </div>
-                <div className="project__box">
 
-                    <img src={img_1} alt="" />
+                <div className="project__box">
+                    <img src={img_5} alt="" />
                     <h3 className="project__title">Optical Character Recognition</h3>
-                    <span className="project__subtitle">0 years Working</span>
+                    <div className="project_made">
+                        <h4 className="project__subtitle">Made with
+                        </h4>
+                        
+                    </div>
+                    <span className="project__subtitle">
+                        I created an Optical Character Recognition (OCR) 
+                        website that used Python for the backend and React 
+                        for the frontend. The website utilized OCR technology 
+                        to convert scanned documents, images, and other types 
+                        of documents into editable and searchable text. I used 
+                        Python libraries such as Tesseract or pytesseract to 
+                        handle the OCR processing, while React handled the 
+                        user interface and user interactions. Overall, my 
+                        project aimed to make it easy for users to extract 
+                        text from images, making it more convenient to edit, 
+                        search or analyze the extracted text.
+                    </span>
                 </div>
-                <div className="project__box">
 
-                    <img src={img_1} alt="" />
+                <div className="project__box">
+                    <img src={img_6} alt="" />
                     <h3 className="project__title">Optical Character Recognition</h3>
-                    <span className="project__subtitle">0 years Working</span>
+                    <div className="project_made">
+                        <h4 className="project__subtitle">Made with
+                        </h4>
+                        
+                    </div>
+                    <span className="project__subtitle">
+                        I created an Optical Character Recognition (OCR) 
+                        website that used Python for the backend and React 
+                        for the frontend. The website utilized OCR technology 
+                        to convert scanned documents, images, and other types 
+                        of documents into editable and searchable text. I used 
+                        Python libraries such as Tesseract or pytesseract to 
+                        handle the OCR processing, while React handled the 
+                        user interface and user interactions. Overall, my 
+                        project aimed to make it easy for users to extract 
+                        text from images, making it more convenient to edit, 
+                        search or analyze the extracted text.
+                    </span>
                 </div>
         </div>
         </section>

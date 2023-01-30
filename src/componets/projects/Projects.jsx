d0efcb6f6f20d14168a641165b72ca541efa8a93
JustinMolina <justinmolina@JustinMolina.local>
2023-01-30 12:45:43 -0500

    Changed the navbar location & updated the 'Scroll To function'

--- src/componets/projects/Projects.jsx
+++ src/componets/projects/Projects.jsx
@@ -9,7 +9,7 @@ import "./projects.css";
 const Projects = () => {
     return (
 
-        <section className="project section" id="about">
+        <section className="project section" id="portfolio">
             <h2 className="section__title">Portfolio</h2>
             <span className="section__subtitle">My Creative Projects</span>
 
